8015d5f1157ae3a3b38fb910d34805e68e37b290
Charlotte Kasin <charlk15@uia.no>
2017-11-07 15:43:09 +0100

    rte

--- server.js
+++ server.js
@@ -1,28 +1,17 @@
 const express = require('express');
 const app = express(); //server-app
 
-// global for all routes -------------------------
-app.use(function(req, res, next) {
+app.get('/', function (req, res) {
 
+  //set headers
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
 
+  //set statusline and body - and send
+  res.status(200).send('Hello World!'); //status-line and body
 
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    next(); //go to the specified route
 });
 
-// -----------------------------------------------
-//route handling is delegated to:
-var travels = require('./travels.js');
-app.use('/travellog/travels/', travels);
-
-//Not yet implemented:
-//var users = require('./users.js');
-//app.use('/travellog/users/', users);
-
-var port = process.env.PORT || 3000;
-
-//------------------------------------------------
-app.listen(port, function () {
-  //console.log('Server listening on port 3000!');
+app.listen(3000, function () {
+  console.log('Server listening on port 3000!');
 });
---
+++ server_bak2.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express(); //server-app
+
+// global for all routes -------------------------
+app.use(function(req, res, next) {
+
+
+
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    next(); //go to the specified route
+});
+
+// -----------------------------------------------
+//route handling is delegated to:
+var travels = require('./travels.js');
+app.use('/travellog/travels/', travels);
+
+//Not yet implemented:
+//var users = require('./users.js');
+//app.use('/travellog/users/', users);
+
+var port = process.env.PORT || 3000;
+
+//------------------------------------------------
+app.listen(port, function () {
+  console.log('Server listening on port 3000!');
+});
