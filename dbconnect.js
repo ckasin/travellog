f2010d94178fbeb3cfcd2bdbab52b59a90b8429b
ckasin <31696899+ckasin@users.noreply.github.com>
2017-11-07 15:48:19 +0100

    Delete dbconnect.js

--- dbconnect.js
+++
@@ -1,8 +0,0 @@
-var pgp = require('pg-promise')();
-
-//db connect string
-var connectstr = process.env.DATABASE_URL || 'postgres://postgres:aa@localhost:5432/travellog';
-
-var db = pgp(connectstr);
-
-module.exports = db;
